08d49f0e67b82ce7e8739cef1feb904798cd0471
abidahmed <md.abid123@gmail.com>
2013-05-06 17:19:55 +0530

    Sample files

---
+++ src/uk/co/wowcher/cashback/web/controller/SampleController.java
@@ -0,0 +1,13 @@
+package uk.co.wowcher.cashback.web.controller;
+
+public class SampleController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
